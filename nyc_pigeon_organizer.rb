cf1cb236a2839dafac7ee65ce9a8bf18117a3f72
Matt Bechtel <bechma02@gmail.com>
2019-10-23 03:14:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,37 @@ def nyc_pigeon_organizer(data)
   require "pp"
 
 
+#create potential values
+  all_colors = data[:color].keys
+  all_genders = data[:gender].keys
+  all_lives = data[:lives].keys
 
+#create array of pigeon names
+  pigeon_names_collection = data[:gender].map { |gender,pigeons|
+    pigeons
+      }
+  pigeon_names = pigeon_names_collection.flatten
+  p pigeon_names
+
+  #prepare standard format
+      standard_format = {
+        color: [],
+        gender: [],
+        lives: []
+      }
+
+#convert name array to a empty hash
+      pigeon_hash = Hash[pigeon_names.collect { |pigeon|
+        [pigeon,standard_format]
+        }]
+
+
+
+
+color_data = map_method(data, :color)
+gender_data = map_method(data, :gender)
+area_data = map_method(data, :lives)
+
+
+  result
 end
