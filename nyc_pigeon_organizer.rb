e7b00886d4916ea45d99b14b00ec11728f07e343
Matt Bechtel <bechma02@gmail.com>
2019-10-23 03:41:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,8 +30,6 @@ all_genders = symbol_to_string(all_genders_sym)
   pigeon_names = pigeon_names_collection.flatten
 
 
-
-
   #prepare standard format
       standard_format = {
         color: [],
@@ -45,6 +43,20 @@ all_genders = symbol_to_string(all_genders_sym)
         }]
 
 puts pigeon_hash
+
+#birds and selectors
+def find_the_pigeon_nonlives(bird, property_to_search, value_to_search, data)
+  for_search = value_to_search.to_sym
+
+  i = 0
+  while i < data[property_to_search][value_to_search].length do
+    if for_search == data[property_to_search][value_to_search][i]
+      return true
+    else
+      i +=1
+    end
+  return false
+end
 
 
   result
